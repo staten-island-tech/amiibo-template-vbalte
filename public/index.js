b20a86e546c7d6ff1113b44add765a1def1f8e96
vbalte <54951038+vbalte@users.noreply.github.com>
2019-12-17 11:46:08 -0500

    okk

--- public/index.js
+++ public/index.js
@@ -2,8 +2,19 @@ const path = require("path"); //take modules and put into ur own skript, templat
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Thami likes bagels and butter. Also he smells");
+// define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+
+//setup handlebars engine and views location
+app.set("views engine", "hbs"); //Telling express/NODE to use handlebars for templates
+app.set("views", viewsPath); //telling express tp get templates from templates/views folder
+app.get("", async (req, res) => {
+  try {
+    res.render("index");
+  } catch (error) {
+    res.status(500).send("Page not found");
+  }
 });
 
 app.get("/thamisucks", (req, res) => {
@@ -12,6 +23,7 @@ app.get("/thamisucks", (req, res) => {
 
 app.listen(3000, (req, res) => {
   console.log("Listening on port 3000");
+  console.log(__dirname);
 });
 
 //127.0.0.1:3000
